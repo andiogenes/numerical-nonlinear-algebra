69287569e0dd0fdd73029ba42730e3092cf8485b
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-05-30 14:14:39 +0700

    Implement command line parsing

--- newton-secant-method/src/lib/newton_secant_method.ex
+++ newton-secant-method/src/lib/newton_secant_method.ex
@@ -2,7 +2,27 @@ defmodule NewtonSecantMethod do
   use Application
 
   def start(_type, _args) do
-    IO.puts "hi"
+    args = parse_args(args_default(), System.argv)
+    IO.puts Map.get(args, :source)
+
     Task.start(fn -> :timer.sleep(1000) end)
+  end
+
+  def args_default do
+    %{:source => "assignment.yml", :destination => "result.yml"}
+  end
+
+  def parse_args(map, argv) do
+    case argv do
+      [] ->
+        map
+      ["source" | [value | tail]] ->
+        parse_args(Map.put(map, :source, value), tail)
+      ["dest" | [value | tail]] ->
+        parse_args(Map.put(map, :destination, value), tail)
+      [option | _] ->
+        IO.puts "Unknown option #{option}"
+        map
+    end
   end
 end
