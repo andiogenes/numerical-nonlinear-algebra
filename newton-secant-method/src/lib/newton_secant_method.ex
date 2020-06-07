5f3c28ad5635bcb51991e026d590dc63926b22c0
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-06-07 14:27:26 +0700

    Write root separation code that doesn't work

--- newton-secant-method/src/lib/newton_secant_method.ex
+++ newton-secant-method/src/lib/newton_secant_method.ex
@@ -2,10 +2,89 @@ defmodule NewtonSecantMethod do
   use Application
 
   def start(_type, _args) do
-    args = parse_args(args_default(), System.argv)
-    IO.puts Map.get(args, :source)
+    # Чтение аргументов
+    args = parse_args(args_default(), System.argv())
+    source = Map.get(args, :source)
+
+    # Чтение конфигурации из YAML
+    {_, config} = YamlElixir.read_from_file(source)
+
+    # Границы отрезка
+    a = config |> Map.get("domain") |> Map.get("a")
+    b = config |> Map.get("domain") |> Map.get("b")
+
+    # Шаг отделения корней
+    step = config |> Map.get("step")
+
+    # "Близость" уточнения корней
+    eps = config |> Map.get("eps")
+
+    # Функция и ее производные
+    fun = config |> Map.get("fun")
+    dfun = config |> Map.get("derivatives") |> Map.get("first")
+    ddfun = config |> Map.get("derivatives") |> Map.get("second")
+
+    # Обертки, исполняющие код, указанный в YAML
+    f = fn x -> Code.eval_string(fun, x: x) |> elem(0) end
+    df = fn x -> Code.eval_string(dfun, x: x) |> elem(0) end
+    ddf = fn x -> Code.eval_string(ddfun, x: x) |> elem(0) end
+
+    roots_intervals = separate_roots(a, b, step, f, df)
+
+    IO.puts roots_intervals
 
     Task.start(fn -> :timer.sleep(1000) end)
+  end
+
+  def separate_roots(a, b, step, f, df) do
+    seq(a, b, step)
+    |> Enum.chunk_every(2, 1, :discard)
+    |> Enum.map(fn [x, y] ->
+      {fx, fy} = {f.(x), f.(y)}
+
+      cond do
+        # Знак функции на концах разный => корни есть
+        sign(fx) != sign(fy) ->
+          # на [x,y] монотонная => корень (возможно кратный)
+          if monotonic?(x, y, step / 4, df) do
+            [x, y]
+          else
+            separate_roots(x, y, step / 2, f, df)
+          end
+
+        # Знак функции одинаковый => возможно корней нет
+        sign(fx) == sign(fy) ->
+          # Монотонная => корней на отрезке нет
+          if monotonic?(x, y, step / 4, df) do
+            nil
+          else
+            separate_roots(x, y, step / 2, f, df)
+          end
+      end
+    end)
+    |> Enum.filter(fn x -> not is_nil(x) end)
+  end
+
+  def seq(a, b, step) do
+    Stream.iterate(a, &(&1 + step))
+    |> Enum.take_while(fn x -> x <= b end)
+  end
+
+  def monotonic?(a, b, step, df) do
+    seq(a, b, step)
+    |> Enum.map(fn x -> sign(df.(x)) end)
+    |> Enum.reduce_while([], fn x, acc ->
+      if x in acc, do: {:halt, 0}, else: {:cont, [x | acc]}
+    end)
+    |> is_list()
+  end
+
+  def sign(x) do
+    cond do
+      x > 0 -> 1
+      x < 0 -> -1
+      true -> 0
+    end
   end
 
   def args_default do
@@ -16,12 +95,15 @@ defmodule NewtonSecantMethod do
     case argv do
       [] ->
         map
+
       ["source" | [value | tail]] ->
         parse_args(Map.put(map, :source, value), tail)
+
       ["dest" | [value | tail]] ->
         parse_args(Map.put(map, :destination, value), tail)
+
       [option | _] ->
-        IO.puts "Unknown option #{option}"
+        IO.puts("Unknown option #{option}")
         map
     end
   end
