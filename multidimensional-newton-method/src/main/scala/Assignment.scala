2a4fd4dd40128cea37584b312996f31d81f390c4
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-06-08 23:10:15 +0700

    Write third test

--- multidimensional-newton-method/src/main/scala/Assignment.scala
+++ multidimensional-newton-method/src/main/scala/Assignment.scala
@@ -5,15 +5,17 @@ object Assignment {
    * Столбец f_i(x)
    */
   val F: DenseMatrix[Vector[Double] => Double] = DenseVector(
-    (x: Vector[Double]) => x.head,
-    (x: Vector[Double]) => x.head
+    (x: Vector[Double]) => Math.sin(x.head),
+    (x: Vector[Double]) => Math.sin(x.head * 2),
+    (x: Vector[Double]) => Math.sin(x.head * 4)
   ).toDenseMatrix.t
 
   /**
    * Матрица Якоби, соответствующая набору функций F.
    */
-  val J: DenseMatrix[Vector[Double] => Double] = DenseMatrix(
-    ((x: Vector[Double]) => x.head, (x: Vector[Double]) => x.head),
-    ((x: Vector[Double]) => x.head, (x: Vector[Double]) => x.head)
-  )
+  val J: DenseMatrix[Vector[Double] => Double] = DenseMatrix((
+    (x: Vector[Double]) => Math.cos(x.head),
+    (x: Vector[Double]) => Math.cos(2 * x.head) * 2,
+    (x: Vector[Double]) => Math.cos(x.head * 4) * 4
+  )).t
 }
