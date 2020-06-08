a4504fe9b2d6a133bcf2db6f57ef81145af5d27a
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-06-08 23:28:21 +0700

    Write fourth test

--- multidimensional-newton-method/src/main/scala/Assignment.scala
+++ multidimensional-newton-method/src/main/scala/Assignment.scala
@@ -5,15 +5,15 @@ object Assignment {
    * Столбец f_i(x)
    */
   val F: DenseMatrix[Vector[Double] => Double] = DenseVector(
-    (x: Vector[Double]) => x.head,
-    (x: Vector[Double]) => x.head
+    (x: Vector[Double]) => x(0) * x(0) + x(1) * x(1) + x(2) * x(2) - 25,
+    (x: Vector[Double]) => x(1) * x(1) - 25,
   ).toDenseMatrix.t
 
   /**
    * Матрица Якоби, соответствующая набору функций F.
    */
   val J: DenseMatrix[Vector[Double] => Double] = DenseMatrix(
-    ((x: Vector[Double]) => x.head, (x: Vector[Double]) => x.head),
-    ((x: Vector[Double]) => x.head, (x: Vector[Double]) => x.head)
+    ((x: Vector[Double]) => 2 * x(0), (x: Vector[Double]) => 2 * x(1), (x: Vector[Double]) => 2 * x(2)),
+    ((x: Vector[Double]) => 0.0, (x: Vector[Double]) => 2 * x(1), (x: Vector[Double]) => 0.0),
   )
 }
