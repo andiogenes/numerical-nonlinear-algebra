6aced7d7b260020468c535d71cacfd0e1933975b
Anton Zavyalov <megadeathlightsaber@gmail.com>
2020-06-11 13:37:36 +0700

    Example 6

--- multidimensional-newton-method/src/main/scala/Assignment.scala
+++ multidimensional-newton-method/src/main/scala/Assignment.scala
@@ -5,15 +5,15 @@ object Assignment {
    * Столбец f_i(x)
    */
   val F: DenseMatrix[Vector[Double] => Double] = DenseVector(
-    (x: Vector[Double]) => x.head,
-    (x: Vector[Double]) => x.head
+    (x: Vector[Double]) => x(0) * x(0) + x(1) * x(1) / 4 - 1,
+    (x: Vector[Double]) => (x(0) + 0.1) * (x(0) + 0.1) + (x(1) + 1) * (x(1) + 1) - 1
   ).toDenseMatrix.t
 
   /**
    * Матрица Якоби, соответствующая набору функций F.
    */
   val J: DenseMatrix[Vector[Double] => Double] = DenseMatrix(
-    ((x: Vector[Double]) => x.head, (x: Vector[Double]) => x.head),
-    ((x: Vector[Double]) => x.head, (x: Vector[Double]) => x.head)
+    ((x: Vector[Double]) => 2 * x(0), (x: Vector[Double]) => x(1) / 2),
+    ((x: Vector[Double]) => 2 * (x(0) + 0.1), (x: Vector[Double]) => 2 * (x(1) + 1))
   )
 }
